9ee137e45a7f8235fbe1f90de823e1ace2f276d8
iantheninja <munene.ian@live.com>
2016-09-10 02:10:57 +0300

    stripe payment model added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  has_one :payment
+  accepts_nested_attributes_for :payment
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
